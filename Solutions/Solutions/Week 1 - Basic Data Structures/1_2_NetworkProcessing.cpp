6b06eb0298936d84495e1006159d89068575c4af
Sean Ziegler <seanziegler@users.noreply.github.com>
2019-08-27 08:51:32 -0400

    Implement packet queue

--- Solutions/Solutions/Week 1 - Basic Data Structures/1_2_NetworkProcessing.cpp
+++ Solutions/Solutions/Week 1 - Basic Data Structures/1_2_NetworkProcessing.cpp
@@ -6,26 +6,26 @@ using namespace std;
 
 void processPackets(const int queueSize, int numberOfPackets, vector<int> &arrivalTime, vector<int> &processingTime) {
 
-	vector<int> packetQueue;
+	vector<int> packetQueue;			// stores finish times of each packet
 	vector<int> processingTimes;
 
 	for (int i = 0; i < numberOfPackets; i++) {
-		if (packetQueue.size != 0 && arrivalTime[i] > packetQueue.front()) {
-			packetQueue.erase(packetQueue.begin());
+		if (packetQueue.size != 0 && arrivalTime[i] > packetQueue.front()) { 	// check if current packet has arrived after queue front has finished
+			packetQueue.erase(packetQueue.begin()); 			// dequeue packets that are already finished
 		}
 		
-		if (packetQueue.size() == 0 || arrivalTime[i] >= packetQueue.back() ) {
-			packetQueue.push_back(arrivalTime[i] + processingTime[i]);
+		if (packetQueue.size() == 0) { 						// if nothing in queue
+			cout << arrivalTime[i] << endl;					// start processing now
+			packetQueue.push_back(arrivalTime[i] + processingTime[i]);	// add to queue until finished
 			
-			cout << arrivalTime[i] << endl;
 		}
-		else if (packetQueue.size() < queueSize) {
-			packetQueue.push_back()
-			cout << queueFinishTime << endl;
-			queueFinishTime += processingTime[i];
+		else if (packetQueue.size() < queueSize) {				// if room left in queue
+			cout << packetQueue.back();					// start processing when final item in queue finishes
+			packetQueue.push_back(packetQueue.back() + processingTime[i])	// update finish time with additional time to process new packet
+			
 		}
 		else {
-			cout << "-1" << endl;
+			cout << "-1" << endl;						// no room left in queue, drop the packet
 		}
 	}
 }
